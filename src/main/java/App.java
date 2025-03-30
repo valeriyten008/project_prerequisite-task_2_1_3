c5911d94a4530c85f0840fbffe095108a8addad9
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-03-30 22:05:38 +0900

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(helloWorld1.getMessage());
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorld1==helloWorld2);
+
+        Cat cat1= (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat1.getMessage());
+        Cat cat2= (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1==cat2);
+
     }
 }
